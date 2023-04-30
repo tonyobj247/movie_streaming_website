47fdaeddf4b2c5245fcded80cefea6437b27e0a9
Tony Obijeih <67270462+tonyobj247@users.noreply.github.com>
2023-04-30 14:12:46 +0100

    Code update
    Work in progress. 
    Next up: Add a bit of JavaScript to the site dynamic.

---
+++ script.js
@@ -0,0 +1,13 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+    arrow.addEventListener("click", () => {
+        movieLists[i].style.transform = translateX(${
+         movieLists[i].computedStyleMap().get("transform")[0].x.value
+         -300px})';
+     });
+ });
+ 
+
+
